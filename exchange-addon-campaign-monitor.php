6291166b55cd003aaf811a4763f80e41364587bf
Thomas Griffin <thomas@thomasgriffinmedia.com>
2013-11-17 18:39:57 -0500

    Update plugin header file comment with proper textdomain.

--- exchange-addon-campaign-monitor.php
+++ exchange-addon-campaign-monitor.php
@@ -15,7 +15,7 @@
  * Version:      1.0.0
  * Author:       Thomas Griffin
  * Author URI:   http://thomasgriffinmedia.com/
- * Text Domain:  tgm-exchange-campaign-monitor
+ * Text Domain:  LION
  * Contributors: griffinjt
  * License:      GPL-2.0+
  * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
